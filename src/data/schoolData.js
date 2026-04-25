af2c94061839160be7c515988da10094d3132e81
dheerajs550 <Dheerajsharma550@gmail.com>
2026-04-25 16:57:21 +0530

    first commit

--- src/data/schoolData.js
+++ src/data/schoolData.js
@@ -4,10 +4,6 @@ const schoolData ={
     tegline:"this is a good school for you",
     contact: "9876543210",
     schNo: "SCH10245",
-    penNo: "PEN45821",
-    apanId: "APN4500123",
-    aadharNo: "4567 8912 3401",
-    accountNo: "14567890023455",
     address: "Indrapuri, Bhopal, Madhya Pradesh",
     image:
       "https://images.unsplash.com/photo-1600880292089-90a7e086ee0c?auto=format&fit=crop&w=800&q=60",
